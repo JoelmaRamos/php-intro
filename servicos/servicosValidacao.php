607ce28566ba6be05f07188604a079a1daaa1ed1
JoelmaRamos <joelmarramos@gmail.com>
2021-07-24 23:08:21 -0300

    Criação de serviços e funçoes PHP

---
+++ servicos/servicosValidacao.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_type =1);
+function validaNome(string $nome): bool
+{
+    if(empty($nome))
+    {
+      setarMensagemErro('O nome não pode ser vazio, por favor preencha-o novamente');
+    return false;
+    }
+    else
+    if(strlen($nome)<4){
+        setarMensagemErro('O nome não pode conter menos de 3 caracteres');
+        return false;
+    }
+    else
+    if(strlen($nome)>40){
+        setarMensagemErro( 'O nome não pode conter mais de 40 caracteres');
+        return false;
+    }
+    return true;
+}
+function validaIdade(string $idade):bool
+{
+    if(!is_numeric($idade)){
+        setarMensagemErro( 'Informe um número para idade');
+return false;
+}
+return true;
+}
+
+
+
